6eb266fe9c3cf62cec525952578ab444d480c55c
jorgelucas <jorgelucasl91@gmail.com>
2021-11-22 18:10:33 -0300

    [FEATURE]  configurando Connection e ConnectionString

---
+++ Business/Interfaces/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Estudos.Dapper.Api.Business.Models;
+
+namespace Estudos.Dapper.Api.Business.Interfaces.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        public List<Usuario> ObterTodos();
+        public Usuario ObterPorId(int id);
+        public void Adicionar(Usuario usuario);
+        public void Atualizar(Usuario usuario);
+        public void Remover(int id);
+    }
+}
---
+++ Infra/Data/Repositories/UsuarioRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Estudos.Dapper.Api.Business.Interfaces.Repositories;
+using Estudos.Dapper.Api.Business.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace Estudos.Dapper.Api.Infra.Data.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly IDbConnection _connection;
+        public UsuarioRepository(IConfiguration configuration)
+        {
+            _connection = new SqlConnection(configuration.GetConnectionString("SqlConnection"));
+        }
+
+        public List<Usuario> ObterTodos()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Usuario ObterPorId(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Adicionar(Usuario usuario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Atualizar(Usuario usuario)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remover(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
