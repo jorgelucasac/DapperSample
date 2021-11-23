44bfcd251f8abb812c8afae5726d358c67dd91cf
jorgelucas <jorgelucasl91@gmail.com>
2021-11-22 23:34:43 -0300

    [REFACTOR] Centralizando as queries de usuário

---
+++ Infra/Data/Repositories/Queries/UsuarioQueries.cs
@@ -0,0 +1,38 @@
+﻿namespace Estudos.Dapper.Api.Infra.Data.Repositories.Queries
+{
+    public static class UsuarioQueries
+    {
+        #region Select
+
+        public static string ObterTodos => "select * from usuarios";
+        public static string ObterPorId => @"SELECT *
+                FROM Usuarios
+                LEFT JOIN Contatos ON Contatos.UsuarioId = Usuarios.Id
+                WHERE Usuarios.Id = @id";
+
+        #endregion
+
+        #region Insert
+
+        public static string AdicionarUsuario => @"INSERT INTO Usuarios
+        (Nome, Email, Sexo, RG, CPF, NomeMae, SituacaoCadastro, DataCadastro)
+        VALUES (@Nome, @Email, @Sexo, @RG, @CPF, @NomeMae, @SituacaoCadastro, @DataCadastro);
+        SELECT CAST(SCOPE_IDENTITY() AS INT);";
+
+        #endregion
+
+        #region Update
+
+        public static string AtualizarUsuario => @"UPDATE Usuarios SET 
+        Nome = @Nome, Email = @Email, Sexo = @Sexo, RG = @RG, CPF = @CPF, NomeMae = @NomeMae,
+        SituacaoCadastro = @SituacaoCadastro, DataCadastro = @DataCadastro WHERE Id = @Id";
+
+        #endregion
+
+        #region Delete
+
+        public static string RemoverUsuario => "DELETE FROM Usuarios WHERE Id = @Id";
+
+        #endregion
+    }
+}
--- Infra/Data/Repositories/UsuarioRepository.cs
+++ Infra/Data/Repositories/UsuarioRepository.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Dapper;
 using Estudos.Dapper.Api.Business.Interfaces.Repositories;
 using Estudos.Dapper.Api.Business.Models;
+using Estudos.Dapper.Api.Infra.Data.Repositories.Queries;
 using Microsoft.Extensions.Configuration;
 
 namespace Estudos.Dapper.Api.Infra.Data.Repositories
@@ -20,19 +21,15 @@ namespace Estudos.Dapper.Api.Infra.Data.Repositories
 
         public async Task<List<Usuario>> ObterTodosAsync()
         {
-            var lista = await _connection.QueryAsync<Usuario>("select * from usuarios");
+            var lista = await _connection.QueryAsync<Usuario>(UsuarioQueries.ObterTodos);
             return lista.ToList();
         }
 
         public async Task<Usuario> ObterPorIdAsync(int id)
         {
-            const string sql = @"SELECT *
-            FROM Usuarios
-            LEFT JOIN Contatos ON Contatos.UsuarioId = Usuarios.Id
-            WHERE Usuarios.Id = @id";
             //o ultimo item é o tipo de retorno
             var lista = await _connection.QueryAsync<Usuario, Contato, Usuario>(
-                 sql,
+                 UsuarioQueries.ObterPorId,
                  //mapeia como os dados devem ser retornados
                  (usuario, contato) =>
                  {
@@ -40,45 +37,24 @@ namespace Estudos.Dapper.Api.Infra.Data.Repositories
                      return usuario;
                  },
                  new { id },
-                 splitOn:"id"
+                 splitOn: "id"
                  );
             return lista.FirstOrDefault();
         }
 
         public async Task<int> AdicionarAsync(Usuario usuario)
         {
-            const string sql = @"INSERT INTO Usuarios
-           (Nome
-           ,Email
-           ,Sexo
-           ,RG
-           ,CPF
-           ,NomeMae
-           ,SituacaoCadastro
-           ,DataCadastro)
-     VALUES
-           (@Nome
-           ,@Email
-           ,@Sexo
-           ,@RG
-           ,@CPF
-           ,@NomeMae
-           ,@SituacaoCadastro
-           ,@DataCadastro); select cast(SCOPE_IDENTITY() as int)";
-
-            return await _connection.QuerySingleAsync<int>(sql, usuario);
+            return await _connection.QuerySingleAsync<int>(UsuarioQueries.AdicionarUsuario, usuario);
         }
 
         public async Task<bool> AtualizarAsync(Usuario usuario)
         {
-            const string sql = "UPDATE Usuarios SET Nome = @Nome, Email = @Email, Sexo = @Sexo, RG = @RG, CPF = @CPF, NomeMae = @NomeMae, SituacaoCadastro = @SituacaoCadastro, DataCadastro = @DataCadastro WHERE Id = @Id";
-            return (await _connection.ExecuteAsync(sql, usuario) > 0);
+            return (await _connection.ExecuteAsync(UsuarioQueries.AtualizarUsuario, usuario) > 0);
         }
 
         public async Task<bool> RemoverAsync(int id)
         {
-            const string sql = "DELETE FROM Usuarios WHERE Id = @Id";
-            return await _connection.ExecuteAsync(sql, new { Id = id }) > 0;
+            return await _connection.ExecuteAsync(UsuarioQueries.RemoverUsuario, new { Id = id }) > 0;
         }
 
         public void Dispose()
